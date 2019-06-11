9301d42c567553e0315ddca9c92c687a9722770d
hgpadovani <hgpadovani@gmail.com>
2019-06-11 16:39:16 -0300

    Adding ML notebook and modifications on utils

--- code/utils.py
+++ code/utils.py
@@ -84,3 +84,118 @@ def get_catcodes(df):
 	    mappers[col] = dict(zip(uniques, df[col].unique()))
 	    
 	return df, mappers
+
+
+def objective_lgbm(params, n_folds = N_FOLDS):
+    """Objective function for Gradient Boosting Machine Hyperparameter Optimization"""
+    
+    # Keep track of evals
+    global ITERATION
+    
+    ITERATION += 1
+    
+    # Retrieve the subsample if present otherwise set to 1.0
+    subsample = params['boosting_type'].get('subsample', 1.0)
+    
+    # Extract the boosting type
+    params['boosting_type'] = params['boosting_type']['boosting_type']
+    params['subsample'] = subsample
+    
+    # Make sure parameters that need to be integers are integers
+    for parameter_name in ['num_leaves', 'subsample_for_bin', 'min_child_samples']:
+        params[parameter_name] = int(params[parameter_name])
+    
+    start = timer()
+    
+    # Perform n_folds cross validation
+    cv_results = lgb.cv(params, train_set, num_boost_round = 10000, nfold = n_folds, 
+                        early_stopping_rounds = 100, metrics = 'auc', seed = 50)
+    
+    run_time = timer() - start
+    
+     # Extract the best score
+    best_score = np.max(cv_results['auc-mean'])
+    
+    # Loss must be minimized
+    loss = 1 - best_score
+    
+    # Boosting rounds that returned the highest cv score
+    n_estimators = int(np.argmax(cv_results['auc-mean']) + 1)
+
+    # Write to the csv file ('a' means append)
+    of_connection = open(out_file, 'a')
+    writer = csv.writer(of_connection)
+    writer.writerow([loss, params, ITERATION, n_estimators, run_time])
+    
+    # Dictionary with information for evaluation
+    return {'loss': loss, 'params': params, 'iteration': ITERATION,
+            'estimators': n_estimators, 
+            'train_time': run_time, 'status': STATUS_OK}
+
+
+def objective(space, n_folds = N_FOLDS):
+    """Objective function for Generic Model Hyperparameter Optimization"""
+    
+    # Keep track of evals
+    global ITERATION
+    
+    ITERATION += 1
+        
+    # KFold cross-validation
+    kf = StratifiedKFold(n_splits = N_FOLDS)
+    
+    # Getting the model
+    model = space['model'](**space['params'])
+
+    # Starting
+    start = timer()
+    
+    # Perform n_folds cross validation
+    roc_aucs = []
+    for train_index, val_index in kf.split(X_train, y_train):
+        X_train_, y_train_ = X_train[train_index, :], y_train[train_index]
+        X_val_, y_val_ = X_train[val_index, :], y_train[val_index]
+
+        # Change the model here
+        model.fit(X_train_, y_train_)
+        roc_aucs.append(roc_auc_score(y_val_, model.predict_proba(X_val_)[:,1]))
+    
+    run_time = timer() - start
+    
+     # Extract the best score
+    best_score = np.mean(roc_aucs)
+    
+    # Loss must be minimized
+    loss = 1 - best_score
+
+    # Write to the csv file ('a' means append)
+    of_connection = open(out_file, 'a')
+    writer = csv.writer(of_connection)
+    writer.writerow([loss, params, ITERATION, run_time])
+    
+    # Dictionary with information for evaluation
+    return {'loss': loss, 'params': params, 'iteration': ITERATION,
+            'train_time': run_time, 'status': STATUS_OK}
+
+def optimize(objective, space, MAX_EVALS = 120, trials, output_file = 'BayesOpt.csv', random_state = 42):
+    
+    # Output file
+    out_file = output_file
+    of_connection = open(out_file, 'w')
+    writer = csv.writer(of_connection)
+
+    # Write the headers to the file
+    writer.writerow(['loss', 'params', 'iteration', 'train_time'])
+    of_connection.close()
+    
+    # Global variable
+    global  ITERATION
+
+    ITERATION = 0
+
+    # Run optimization
+    best = fmin(fn = objective, space = space, algo = tpe.suggest, 
+                max_evals = MAX_EVALS, trials = trials, rstate = random_state)
+    
+    return space_eval(space, best)
+    
---
+++ spaces/search_spaces.py
@@ -0,0 +1,58 @@
+# search space for hyperparameter optimization
+
+from hyperopt import hp
+from sklearn.ensemble import RandomForestClassifier, GradientBoostingClassifier
+import XGBoost as xgb
+import LightGBM as lgb
+
+
+xgb_space = {'model': xgb.XGBClassifier,
+             'params': {
+                        'n_estimators' : hp.quniform('xgb_n', 1000, 5000, 500),
+                        'learning_rate' : hp.normal('xgb_eta', 0.05, 0.01),
+                        'max_depth' : hp.quniform('xgb_max_depth', 2, 8, 1),
+                        'min_child_weight' : hp.quniform('xgb_min_child_weight', 1, 6, 1),
+                        'subsample' : hp.uniform('xgb_subsample', 0.2, 1),
+                        'gamma' : hp.uniform('xgb_gamma', 0.01, 0.4),
+                        'colsample_bytree' : hp.uniform('xgb_colsample_bytree', 0.2, 1),
+                        'objective': hp.choice('xgb_obj', ['binary:logistic']),
+                        'scale_pos_weight': hp.uniform('xgb_w', 1.0, 5.0)
+                        }
+            }
+
+lgm_space = {'model': lgm.LGBMClassifier,
+             'params': {
+                      'class_weight': hp.choice('class_weight', [None, 'balanced']),
+                      'boosting_type': hp.choice('boosting_type', [{'boosting_type': 'gbdt', 'subsample': hp.uniform('gdbt_subsample', 0.5, 1)}, 
+                                                                   {'boosting_type': 'dart', 'subsample': hp.uniform('dart_subsample', 0.5, 1)},
+                                                                   {'boosting_type': 'goss', 'subsample': 1.0}]),
+                      'num_leaves': hp.quniform('num_leaves', 30, 150, 1),
+                      'learning_rate': hp.loguniform('learning_rate', np.log(0.01), np.log(0.2)),
+                      'subsample_for_bin': hp.quniform('subsample_for_bin', 20000, 300000, 20000),
+                      'min_child_samples': hp.quniform('min_child_samples', 20, 500, 5),
+                      'reg_alpha': hp.uniform('reg_alpha', 0.0, 1.0),
+                      'reg_lambda': hp.uniform('reg_lambda', 0.0, 1.0),
+                      'colsample_bytree': hp.uniform('colsample_by_tree', 0.6, 1.0)
+                        }
+            }
+
+rf_space = {'model': RandomForestClassifier,
+            'params': {
+                      'max_depth': hp.choice('max_depth', range(1,20)),
+                      'max_features': hp.choice('max_features', range(1,150)),
+                      'n_estimators': hp.choice('n_estimators', range(100,500)),
+                      'criterion': hp.choice('criterion', ["gini", "entropy"])
+                      }
+            }
+
+
+gbc_space = {'model': GradientBoostingClassifier,
+			 'params': {
+					'learning_rate': hp.uniform( 'lr', 0.01, 0.2 ),
+					'subsample': hp.uniform( 'ss', 0.8, 1.0 ),
+					'max_depth': hp.quniform( 'md', 2, 10, 1 ),
+					'max_features': hp.choice( 'mf', ( 'sqrt', 'log2', None )),
+					'min_samples_leaf': hp.quniform( 'mss', 1, 10, 1 ),
+					'min_samples_split': hp.quniform( 'mss', 2, 20, 1 )
+					}
+			}
